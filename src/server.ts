f9171bd04d74c223ca64706b751638deb8525829
Alex Xavier <alexjr.123@hotmail.com>
2024-04-03 21:51:37 -0300

    new: add probes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
-import fastify from "fastify";
+import fastify, { FastifyInstance } from "fastify";
 
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUI from "@fastify/swagger-ui";
 import fastifyCors from "@fastify/cors";
 
-import { serializerCompiler, validatorCompiler, jsonSchemaTransform, ZodTypeProvider } from 'fastify-type-provider-zod'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
@@ -13,41 +18,44 @@ import { checkIn } from "./routes/check-in";
 import { getEventAttendees } from "./routes/get-event-attendees";
 import { errorHandler } from "./error-handler";
 
-export const app = fastify().withTypeProvider<ZodTypeProvider>()
+export const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-  origin: '*',
-})
+  origin: "*",
+});
 
 app.register(fastifySwagger, {
   swagger: {
-    consumes: ['application/json'],
-    produces: ['application/json'],
+    consumes: ["application/json"],
+    produces: ["application/json"],
     info: {
-      title: 'pass.in',
-      description: 'Especificações da API para o back-end da aplicação pass.in construída durante o NLW Unite da Rocketseat.',
-      version: '1.0.0'
+      title: "pass.in",
+      description:
+        "Especificações da API para o back-end da aplicação pass.in construída durante o NLW Unite da Rocketseat.",
+      version: "1.0.0",
     },
   },
   transform: jsonSchemaTransform,
-})
+});
 
 app.register(fastifySwaggerUI, {
-  routePrefix: '/docs',
-})
+  routePrefix: "/docs",
+});
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.register(createEvent)
-app.register(registerForEvent)
-app.register(getEvent)
-app.register(getAttendeeBadge)
-app.register(checkIn)
-app.register(getEventAttendees)
+app.register(createEvent);
+app.register(registerForEvent);
+app.register(getEvent);
+app.register(getAttendeeBadge);
+app.register(checkIn);
+app.register(getEventAttendees);
+app.register((app: FastifyInstance) => {
+  app.get("/healthz", async (req, res) => res.status(200).send());
+});
+app.setErrorHandler(errorHandler);
 
-app.setErrorHandler(errorHandler)
-
-app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-  console.log('HTTP server running!')
-})
+app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
+  console.log("HTTP server running!");
+});
